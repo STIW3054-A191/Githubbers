d17cdcb56c5da28ad3cd44530a656d3409226af6
muhdhariz <muhdhariz689@gmail.com>
2019-12-26 16:36:30 +0800

    update pom.xml

---
+++ src/main/java/com/github/Githubbers/mainPlug.java
@@ -0,0 +1,15 @@
+package com.github.Githubbers;
+
+public class mainPlug {
+
+    public static void main(String[] args) {
+
+        new mainPlug().getPlug();
+    }
+
+    private void getPlug() {
+
+
+
+    }
+}
