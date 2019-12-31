aac9193c95334622c904a22440845258895e5445
macasyraf <macasyraf@gmail.com>
2019-12-31 22:56:49 +0800

    grab repo

---
+++ src/main/java/com/github/macasyraf/mainPlug.java
@@ -0,0 +1,14 @@
+package com.github.macasyraf;
+
+public class mainPlug {
+
+    public static void main(String[] args) {
+
+        new mainPlug().getPlug();
+    }
+
+    private void getPlug() {
+
+    }
+
+}
