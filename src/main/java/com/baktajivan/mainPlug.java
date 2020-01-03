a7844d0ef3f403a3b90aef95d2bb06379cc9712e
Baktajivan Pillay 252709 <baktajivan98@gmail.com>
2020-01-04 00:09:17 +0800

    Add files via upload
    Clone Repo

---
+++ src/main/java/com/baktajivan/mainPlug.java
@@ -0,0 +1,17 @@
+package com.baktajivan;
+
+public class mainPlug {
+
+    public static void main(String[] args) {
+
+        //new mainPlug().getPlug();
+        CloneRepo clone = new CloneRepo();
+
+
+    }
+
+    private void getPlug() {
+
+    }
+
+}
---
+++ src/main/java/com/baktajivan/repoPlug.java
@@ -0,0 +1,40 @@
+package com.baktajivan;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+class repoPlug {
+
+    public ArrayList<String> getLink() {
+
+        ArrayList<String> arrayList = new ArrayList<>();
+        try {
+
+            final Document document = Jsoup.connect("https://github.com/STIW3054-A191/Assignments/issues/1").get();
+            Elements elements = document.select("table").select("a");
+
+            for (Element array : elements) {
+                String elementLink = array.attr("href");
+
+                if (elementLink.endsWith(".git")) {
+                    arrayList.add(elementLink.substring(0, elementLink.length() - 4));
+                } else {
+                    arrayList.add(elementLink);
+                }
+            }
+
+        } catch (IOException ie) {
+            ie.printStackTrace();
+        }
+
+        return arrayList;
+
+
+    }
+
+}
